392119eb7e708d8f7f1f04f9c68dd7ed61580a56
Ben Greear <greearb@candelatech.com>
2016-07-27 14:33:55 -0700

    ath10k:  Allow configuring phy-xretry-threshold for CT firmware.
    The firmware will reset the wifi chipset after X consecutive
    failed retries.  Default is 0x40 for 10.1 firmware.  CT firmware
    rls 17 and higher allows configuring this threshold.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.c
+++ drivers/net/wireless/ath/ath10k/core.c
@@ -1024,6 +1024,10 @@ int ath10k_core_start(struct ath10k *ar, enum ath10k_firmware_mode mode)
 		if (ar->eeprom_overrides.tx_sta_bw_mask)
 			ath10k_wmi_pdev_set_special(ar, SET_SPECIAL_ID_STA_TXBW_MASK,
 						    ar->eeprom_overrides.tx_sta_bw_mask);
+
+		if (ar->eeprom_overrides.pdev_xretry_th)
+			ath10k_wmi_pdev_set_special(ar, SET_SPECIAL_ID_PDEV_XRETRY_TH,
+						    ar->eeprom_overrides.pdev_xretry_th);
 	}
 
 	if (test_bit(ATH10K_FW_FEATURE_WMI_10X_CT, ar->fw_features)) {
